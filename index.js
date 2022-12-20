d9a883bce729651bd5fb737c25465522341ad564
brmrodrigues <lpbruno12@gmail.com>
2022-12-19 22:55:57 -0300

    filter (independently) by name, state and from/to

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+const { statesByCode, statesByName } = require('./states.js');
+
 const express = require('express');
 
 const app = express();
 
+const dentalClinicsUrl = 'https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json';
+const vetClinicsUrl = 'https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json';
+
 app.get('/search', async (req, res) => {
-  const name = req.query.name;
-  const state = req.query.state;
-  const from = req.query.from;
-  const to = req.query.to;
+  const queryName = req.query.name;
+  const queryState = req.query.state;
+  const fromQuery = req.query.from;
+  const toQuery = req.query.to;
 
   try {
     let [dentalClinics, vetClinics] = await Promise.all([
-      fetch('https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json').then(r => r.json()),
-      fetch('https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json').then(r => r.json())
+      fetch(dentalClinicsUrl).then(r => r.json()),
+      fetch(vetClinicsUrl).then(r => (r.json()))
     ]);
-    res.send(vetClinics);
+    const updatedVetClinics = vetClinics.map(({clinicName: name,
+                                               stateCode: stateName,
+                                               opening: availability}) => ({name, stateName, availability}));
+    const allClinics = dentalClinics.concat(updatedVetClinics);
+
+    const filteredClinics = allClinics.filter(({name, stateName, availability}) => {
+      if (queryName && name != queryName) {
+        return false;
+      }
+      if (queryState && !(stateName == queryState || queryState == statesByCode[stateName] || queryState == statesByName[stateName])) {
+        return false;
+      }
+      if (fromQuery && toQuery && !(availability.from <= fromQuery && availability.to >= toQuery)) {
+        return false;
+      }
+      return true;
+    });
+
+    res.send(filteredClinics);
   }
   catch(error) {
     console.log(error);
